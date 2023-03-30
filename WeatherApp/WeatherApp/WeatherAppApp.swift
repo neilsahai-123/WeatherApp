bd5f5f273f79c6e933cdd325f06475c038aea2b2
Neil Sahai <neilsahai123@gmail.com>
2023-03-30 11:18:16 +0530

    Initial Commit - Project Setup for Weather App

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Neil Sahai on 30/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
