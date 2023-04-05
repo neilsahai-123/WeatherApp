d904b87c2f7f02a76501e834ceac457deb2edfa9
Neil Sahai <neilsahai123@gmail.com>
2023-04-05 22:02:40 +0530

    Added API response and and API and Network manager to interact with weather APIs

---
+++ WeatherApp/WeatherApp/View/MenuHeaderView.swift
@@ -0,0 +1,47 @@
+//
+//  MenuHeaderView.swift
+//  WeatherApp
+//
+//  Created by Neil Sahai on 03/04/23.
+//
+
+import SwiftUI
+
+struct MenuHeaderView: View {
+    
+    @ObservedObject var cityVM: CityViewModal
+    @State private var searchTerm = "New York"
+    
+    var body: some View {
+        HStack {
+            TextField("", text: $searchTerm)
+                .padding(.leading, 20)
+            
+            Button {
+                cityVM.city = searchTerm
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .fill(Color.blue)
+                    Image(systemName: "location.fill")
+                }
+            }.frame(width: 50, height: 50)
+        }.foregroundColor(.white)
+            .padding()
+            .background {
+                ZStack(alignment: .leading) {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.white)
+                        .padding(.leading, 10.0)
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.blue.opacity(0.5))
+                }
+            }
+    }
+}
+
+struct MenuHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuHeaderView(cityVM: CityViewModal())
+    }
+}
---
+++ WeatherApp/WeatherApp/ViewModel/CityViewModal.swift
@@ -0,0 +1,13 @@
+//
+//  CityViewModal.swift
+//  WeatherApp
+//
+//  Created by Neil Sahai on 03/04/23.
+//
+
+import Foundation
+
+final class CityViewModal: ObservableObject {
+    @Published var city: String = "San Francisco"
+    
+}
